47f02bb362f31d5fa7e6af2f325b979028981496
HericMendez <heric.mendez00@gmail.com>
2025-05-02 22:52:00 -0300

    fixed paths for github pages

--- scripts/footer.script.js
+++ scripts/footer.script.js
@@ -1,5 +1,5 @@
 import { footer } from "../components/footer.js";
 
-const footerDiv = document.getElementById('footerDiv');
+const footerDiv = document.getElementById("footerDiv");
 
 footerDiv.innerHTML = footer();
--- scripts/orderPlaced.js
+++ scripts/orderPlaced.js
@@ -1,3 +1,3 @@
 setTimeout(() => {
-    window.location.href = "../index.html"
+    window.location.href = "index.html";
 }, 4200);
